b6b9e59f86f003d0c3deaac0649a0f4c5f5cd9d3
GiangMinhOfficial <ghieuming@gmail.com>
2025-01-15 14:36:08 +0700

    add GetProductByCategory api, UpdateProduct api

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (ISender sender, string category) =>
+        {
+            var results = await sender.Send(new GetProductByCategoryQuery(category));
+
+            var products = results.Adapt<GetProductByCategoryResponse>();
+
+            return Results.Ok(products);
+        })
+        .WithName("GetProductByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get ProductCategory")
+        .WithDescription("Get ProductCategory");
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,6 +1,23 @@
-﻿namespace Catalog.API.Products.GetProductById
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
 {
-    public class GetProductByIdEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/products/{id:guid}", async (ISender sender, Guid id) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get ProductId")
+        .WithDescription("Get ProductId");
     }
 }
