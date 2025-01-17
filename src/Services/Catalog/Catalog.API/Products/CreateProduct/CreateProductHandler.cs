70be0c01b1a0443d421d81f2898ea8cc909b553a
GiangMinhOfficial <ghieuming@gmail.com>
2025-01-17 08:44:21 +0700

    config fluent validation in mediatr pipeline

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -2,14 +2,32 @@
 
 public record CreateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
-
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session)
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+        RuleFor(x => x.Price).NotEmpty().WithMessage("Price must be greater than 0");
+    }
+}
+
+internal class CreateProductCommandHandler
+    (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        //var result = await validator.ValidateAsync(command, cancellationToken);
+
+        //if (result.Errors.Any())
+        //    throw new ValidationException(result.Errors.FirstOrDefault()?.ErrorMessage ?? string.Empty);
+
+        logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
         var product = new Product()
         {
             Name = command.Name,
